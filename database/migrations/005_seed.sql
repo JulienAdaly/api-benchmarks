628a5e32baaef9a98f4d41d9b5d47efe00693600
Julien C. <julien@adaly.fr>
2025-10-21 16:35:55 +0000

    chore(migrations): seed database

---
+++ database/migrations/005_seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (id,username,email,password_hash,bio,created_at,is_admin) VALUES
+	 ('09297c10-5a75-4f4c-91ed-aa40abe5b055'::uuid,'admin','admin@admin.fr','$2b$08$WQZlxDJ5MmT7wSXFTy2UU.JMwzc0/ixWecj528vRkl/FmBye3I6tO',NULL,'2025-10-20 14:39:34.627009+02',true);
